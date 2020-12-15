7ed27e4c4e1c004a354016dc5365dfde6269303b
quansenB <inaki.breinbauer@gmail.com>
2020-12-15 11:03:59 +0100

    change back to img folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => (
     <p /* style={{fontFamily:"Rubik", fontWeight:600}} */ class="font-one font-semibold font-sm">Welcome to your new Gatsby site.</p>
     <p /* style={{fontFamily:"Andika New Basic", fontWeight:400}} */ class="font-two">Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <StaticImage src="../../static/panda.jpg" loading="eager" alt="Gatsby Panda" layout="fluid" class="w-full h-auto" />
+      <StaticImage src="../images/panda.jpg" loading="eager" alt="Gatsby Panda" layout="fluid" class="w-full h-auto" />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
