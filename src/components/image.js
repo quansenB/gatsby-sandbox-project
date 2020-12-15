5dc2dc0c83b9ff0c54f72fffee0e0bbf348fd0e3
quansenB <inaki.breinbauer@gmail.com>
2020-12-15 09:40:01 +0100

    add loading eager

--- src/components/image.js
+++ src/components/image.js
@@ -13,7 +13,7 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-const Image = () => {
+const Image = (props) => {
   const data = useStaticQuery(graphql`
     query {
       placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
@@ -30,7 +30,7 @@ const Image = () => {
     return <div>Picture not found</div>
   }
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img loading={props.loading} fluid={data.placeholderImage.childImageSharp.fluid} />
 }
 
 export default Image
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1 /* style={{fontFamily:"Rubik", fontWeight:400}} */ class="font-one">Hi people</h1>
-    <p /* style={{fontFamily:"Rubik", fontWeight:600}} */ class="font-one font-semibold">Welcome to your new Gatsby site.</p>
+    <p /* style={{fontFamily:"Rubik", fontWeight:600}} */ class="font-one font-semibold font-sm">Welcome to your new Gatsby site.</p>
     <p /* style={{fontFamily:"Andika New Basic", fontWeight:400}} */ class="font-two">Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <Image loading="eager"/>
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
