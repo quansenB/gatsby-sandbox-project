3a5a3851feaba632821286ef2c8dd92485ce3479
quansenB <inaki.breinbauer@gmail.com>
2020-12-15 10:50:27 +0100

    add preact and static image

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-preload-fonts`,
+    "gatsby-plugin-preact",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -14,6 +15,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    "gatsby-plugin-image",
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -28,7 +30,22 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          "/": [
+            "Link: </static/my-logo.png>; rel=preload; as=image",
+          ],
+        }, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
     'gatsby-plugin-postcss',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-
-
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => (
   <Layout>
@@ -15,7 +13,7 @@ const IndexPage = () => (
     <p /* style={{fontFamily:"Rubik", fontWeight:600}} */ class="font-one font-semibold font-sm">Welcome to your new Gatsby site.</p>
     <p /* style={{fontFamily:"Andika New Basic", fontWeight:400}} */ class="font-two">Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image loading="eager"/>
+      <StaticImage src="../../static/gatsby-astronaut.png" loading="eager" alt="3-Schritte-Strategie-Finally-Freelancing" layout="fluid" class="w-full h-auto" />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
